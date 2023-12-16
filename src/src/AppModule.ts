b2b9ffb82ca56fa91e78752c070c3fb9ba58d3a4
fujivara <marat.skyba@gmail.com>
2023-12-17 00:26:52 +0200

    Cleanup database connection

--- src/src/AppModule.ts
+++ src/src/AppModule.ts
@@ -1,44 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Answer } from './db/schemas/Answer';
-import { Grant } from './db/schemas/Grant';
-import { OptionAnswer } from './db/schemas/OptionAnswer';
-import { Option } from './db/schemas/Option';
-import { Permission } from './db/schemas/Permission';
-import { Question } from './db/schemas/Question';
-import { Role } from './db/schemas/Role';
-import { Survey } from './db/schemas/Survey';
-import { TextAnswer } from './db/schemas/TextAnswer';
-import { User } from './db/schemas/User';
 import { QuestionModule } from './modules/QuestionModule';
 import { DatabaseModule } from './modules/DatabaseModule';
 import { SurveyModule } from './modules/SurveyModule';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'shin66',
-    port: 3306,
-    database: 'mydb',
-    entities: [
-      Answer,
-      Grant,
-      Option,
-      OptionAnswer,
-      Permission,
-      Question,
-      Role,
-      Survey,
-      TextAnswer,
-      User,
-    ],
-    synchronize: true,
-  }),
-  QuestionModule,
-  DatabaseModule,
-  SurveyModule,
+  imports: [
+    QuestionModule,
+    DatabaseModule,
+    SurveyModule,
   ],
 })
 export class AppModule {}
